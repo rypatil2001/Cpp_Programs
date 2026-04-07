2c2fbd6fe91ffa03a27e24847cee34c23eafcbce
Raj Patil <rypatil2001@gmail.com>
2026-04-06 18:40:05 -1000

    Enhance comments and initialize A in constructor
    Added a comment for the default constructor and initialized non-static characteristic A.

--- 11_Static_Behaviour.cpp
+++ 11_Static_Behaviour.cpp
@@ -7,6 +7,7 @@ class Demo
         int A, B;           // Non static characteristics
         static int X, Y;    // Static characteristics
 
+
         Demo()              // Default costructor 
         {
             // Initialisation of non static characteristics
